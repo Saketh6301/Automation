d44178062480dd75d69a3c60bd50fb7ea65c1497
Saketh6301 <37059934+Saketh6301@users.noreply.github.com>
2018-03-27 00:17:25 +0530

    1

---
+++ com.learnautomation.selenium/src/test/java/com/learnautomation/selenium/DemoForMaven.java
@@ -0,0 +1,15 @@
+package com.learnautomation.selenium;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+
+
+public class DemoForMaven 
+{
+@Test
+	public void testHello()
+	{
+	System.out.println("Hello");	
+	}
+}
